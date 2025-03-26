b4603bdcd45682e3a6dc3129196909184172d0fa
DEEPIKAMALL15 <deepikam1508@gmail.com>
2025-03-26 15:33:27 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,8 +2,25 @@ const express = require ('express');
 
 const app= express();
 
-app.use((req, res) => {
-    res.send("Hello from the server");
+/* app.get("/user/:userId/:name:/password",(req, res) => {
+    console.log(req.params);
+    res.send({firstname: "Akshay", lastname: "Saini"});
+});
+app.get("/test",(req,res)=>{
+    res.send("Test");
+}) */
+//app.use("/route",rH, [rH2,rH3] , rH4)
+app.use("/user", (req,res,next)=> {
+    //Route handler
+    console.log("Handling the route 1");
+    //res.send("Route Handler 1");
+    next();
+
+},
+(req,res)=>{
+    //another route handler
+    console.log("Handling the route 2");
+    res.send("Route Handler 2");
 });
 
 app.listen(3000,()=>{
