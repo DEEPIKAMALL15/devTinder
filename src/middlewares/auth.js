352d5f51ea4b98fe29ca7239e222f430be94a59f
DEEPIKAMALL15 <deepikam1508@gmail.com>
2025-06-20 21:45:49 +0530

    new jwt

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,4 @@
- const jwt = require ('jsonwebtoken');
- const User = require ("../models/user.js");
+ 
  /* const adminAuth = (req,res,next) => {
     console.log("Admin Auth is getting checked!")
     const token ="xyz";
