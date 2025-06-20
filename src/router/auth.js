d7c71f2ddb85b03d3c1b8aa0aa1b6d1f26c1c3fc
DEEPIKAMALL15 <deepikam1508@gmail.com>
2025-06-20 21:53:28 +0530

    token update

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const authRouter = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require ('jsonwebtoken');
@@ -8,9 +8,9 @@ const User = require ("../models/user.js");
 authRouter.post("/signup",async(req,res)=>{
     
     try{
-        //validate the data
+       
         validateSignUpData(req);
-        //encrypt the password
+        
         const {firstName,lastName,emailId,password} = req.body;
         const passwordHash = await bcrypt.hash(password,10);
         const user = new User ({
@@ -21,7 +21,7 @@ authRouter.post("/signup",async(req,res)=>{
         });
         const savedUser = await user.save();
         const token = await savedUser.getJWT();
-            //Add token to cookie and send reply back to user
+           
             res.cookie("token",token,{expires:new Date(Date.now()+8 * 3600000)});
         
         res.json({message: "User Added Successfully!", data: savedUser});
@@ -39,9 +39,9 @@ authRouter.post("/login",async (req,res)=>{
         }
         const isPasswordValid = await user.validatePassword(password);
         if(isPasswordValid){
-            // create a jwt token
+            
             const token = await user.getJWT();
-            //Add token to cookie and send reply back to user
+            
             res.cookie("token",token,{expires:new Date(Date.now()+8 * 3600000)});
             res.send(user);
         }
@@ -59,5 +59,72 @@ authRouter.post("/logout", async (req,res)=>{
     });
     res.send("Logout Successfully !!!");
 })
+
+module.exports = authRouter;
+ */
+const express = require('express');
+const authRouter = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const { validateSignUpData } = require("../utils/validator");
+const User = require("../models/user.js");
+
+authRouter.post("/signup", async (req, res) => {
+  try {
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      expires: new Date(Date.now() + 8 * 3600000)
+    });
+    res.json({ message: "User Added Successfully!", data: savedUser });
+  } catch (err) {
+    res.status(400).send("Error in saving data" + err);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId });
+    if (!user) throw new Error("Invalid credentials");
+    const isPasswordValid = await user.validatePassword(password);
+    if (isPasswordValid) {
+      const token = await user.getJWT();
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        expires: new Date(Date.now() + 8 * 3600000)
+      });
+      res.send(user);
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("Error: " + err);
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    expires: new Date(Date.now())
+  });
+  res.send("Logout Successfully !!!");
+});
 
 module.exports = authRouter;
